65828b687aa940bd39d90ba64ee07ca06d4e8f51
Abdul Rehman <129746474+Abdul-Rehman-13812@users.noreply.github.com>
2023-08-05 21:05:46 +0500

    Task 1 to Task 5

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+//Task 2
+const personname = "Eric";
+console.log("Hello Eric, would you like to learn some Python today?");
+//task 3
+const personname1 = "AbdulRehman";
+const lowercasesrting = personname1.toLowerCase();
+const uppercasestring = personname1.toUpperCase();
+console.log(lowercasesrting);
+console.log(uppercasestring);
+//Task 4
+const authoename = "The Stephen King once said,";
+const quote = "I try to create sympathy for my characters, then turn the monsters loose. ";
+console.log(`${authoename}  "${quote}"`);
+//task 5
+const famous_person = "Ernest Hemingway once said ,";
+const message = "Prose is architecture, not interior decoration.";
+console.log(`${famous_person}" ${message}"`);
