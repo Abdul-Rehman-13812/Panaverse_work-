413d7171ec425d9b1f929e87972f66489e3338d1
Abdul Rehman <129746474+Abdul-Rehman-13812@users.noreply.github.com>
2023-08-12 20:29:24 +0500

    add questions upto 14

--- index.js
+++ index.js
@@ -41,3 +41,31 @@ let trimpersonname1 = personname.trim();
 console.log("Personname with trim Whitespaces  ");
 console.log(trimpersonname1);
 console.log("*------------*");
+//Question no 11
+let namesarray = ["AbdulRehman", "Abdullah ", "Abdul raheem  ", "AbdulKareem", "ETC"];
+console.log(namesarray);
+console.log("*------------*");
+//Question no 12
+const namesarray1 = ["AbdulRehman", "Abdullah ", "Abdul raheem  ", "AbdulKareem", "ETC"];
+const messages = "Salam, {name}!Where are you from   ";
+for (const name of namesarray1) {
+    const personalizedmessage = messages.replace("{name}", name);
+    console.log(personalizedmessage);
+}
+console.log("*------------*");
+//Question no 13
+const carsnames = ["Toyota", "Honda", "Suzuki", "Tesla", "PArado"];
+const messages1 = "I would like {name} car   ";
+for (const name of carsnames) {
+    const personalizedmessage1 = messages1.replace("{name}", name);
+    console.log(personalizedmessage1);
+}
+console.log("*------------*");
+//Question no 14
+const guestlist = ["Ali", "Abdul rehman", "Abdullah"];
+const message2 = "Salam, {name}! you are invited for  dinner this night. ";
+for (const name of guestlist) {
+    const personalizedmessage2 = message2.replace("{name}", name);
+    console.log(personalizedmessage2);
+}
+console.log("*------------*");
